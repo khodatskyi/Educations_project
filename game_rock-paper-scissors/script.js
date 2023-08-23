ca332f97903ba74e0da82608083a88ad353ffa77
Dmytro Khodatskyi <khodatskyidmytro@gmail.com>
2023-08-23 18:46:01 +0300

    released the full version of the game

--- game_rock-paper-scissors/script.js
+++ game_rock-paper-scissors/script.js
@@ -1,61 +1,97 @@
+// Работа с ДОМ
+
+const paperButton  = document.getElementById("paper")
+const scissorsButton = document.getElementById("scissors")
+const rockButton = document.getElementById("rock")
+const result = document.getElementById('result')
+const playerScoreLink = document.getElementById("playerScores")
+const compScoreLink = document.getElementById("compScores")
+const compImg = document.getElementById("comp_img")
+
+const yourChoose = document.getElementById("yourChoose")
+const yourChooseText = yourChoose.textContent // Здесь хранится текст Your choose is 
+
+const compChoose = document.getElementById("compChoose")
+const compChooseText = compChoose.textContent // Здесь хранится текст Computer choose is
+
+let playerScores = 0
+let compScores = 0
+
+
+paperButton.addEventListener("click", () => {
+    yourChoose.textContent = yourChooseText + 'paper'
+    game("paper")
+});
+scissorsButton.addEventListener("click", () => {
+    yourChoose.textContent = yourChooseText + 'scissors'
+    game("scissors")
+});
+rockButton.addEventListener("click", () => {
+    yourChoose.textContent = yourChooseText + 'rock'
+    game("rock")
+});
+
+
+
 // Даем компьюету выбрать камень, ножницы или бумагу
-// Возвращает выбор компьютера (камень, ножницы или бумагу)
+// Возвращает выбор компьютера (камень, ножницы или бумагу) + обновляет строку выбора + обновляет картинку
 const getComputerChoice = () => {
     const choice = Math.random().toFixed(3)
     if (choice <= 0.333) {
+        compChoose.textContent = compChooseText + 'scissors'
+        compImg.src = 'img/scissors.png'
         return 'scissors'
     } else if (choice >= 0.333 & choice <= 0.666) {
+        compChoose.textContent = compChooseText + 'paper'
+        compImg.src = 'img/paper.png'
         return 'paper'
-    } else return 'rock'
+    } else {
+        compChoose.textContent = compChooseText + 'rock'
+        compImg.src = 'img/stone.png'
+        return 'rock'
+    }
 }
 
-// Даем игроку выбрать камень, ножницы или бумагу
-// Возвращает выбор игрока (камень, ножницы или бумагу)
-const getPlayerSelection = () => {
-    const playerSelection = prompt('scissors, paper or rock?')
-    const playerSelectionToLower = playerSelection.toLowerCase()
-    if (playerSelectionToLower !== 'scissors' && playerSelectionToLower !== 'paper' && playerSelectionToLower !== 'rock') {
-        return 'Вы ввели неправильное значение!'
-    } else return playerSelectionToLower
+// Когда будет набрано 5 очков, то игра будет обновлена
+const gameOver = () => {
+    compScores = 0
+    playerScores = 0
+    playerScoreLink.textContent = `scores ${playerScores}` 
+    compScoreLink.textContent =  `scores ${compScores}` 
+    return compScores, playerScores, playerScoreLink.textContent, compScoreLink.textContent
 }
+
 
 // Опеределяем победителя
 const game = (playerSelection, computerSelection) => {
     const comp = getComputerChoice() // (камень, ножницы или бумагу)
-    const player = getPlayerSelection() // (камень, ножницы или бумагу)
-
+    const player = playerSelection
+ 
     if (comp === player) {
-        return console.log('ничья')
+        return result.textContent = `Ничья!`
     } else if (
         comp === 'rock' && player === 'scissors' ||
         comp === 'scissors' && player === 'paper' ||
         comp === 'paper' && player === 'rock') {
-        return console.log('Выиграл компьютер')
+        compScores++
+        compScoreLink.textContent =  `scores ${compScores}`  
+        if (compScores == 5) { 
+            gameOver()
+            return result.textContent = `В этом матче выиграл Компьютер!`
+        }
+        return result.textContent = 'Ты проиграл!'
     } else if (
         comp === 'rock' && player === 'paper' ||
         comp === 'scissors' && player === 'rock' ||
         comp === 'paper' && player === 'scissors') {
-        return console.log('Выиграл игрок')
+        playerScores++    
+        playerScoreLink.textContent = `scores ${playerScores}`  
+        if (playerScores == 5) { 
+            gameOver()
+            return result.textContent = `В этом матче Ты выиграл!`
+        }
+        return result.textContent = 'Ты выиграл!'
     }
 }
-
-// game()
-
-// Тестировка нашей игры
-    // let wins = 0;
-    // let losses = 0;
-    // let draws = 0;
-
-
-
-    // // Запуск 1000 итераций игры
-    // for (let i = 0; i < 1000; i++) {
-    //     game();
-    // }
-
-    // console.log(`Победы: ${wins}`);
-    // console.log(`Поражения: ${losses}`);
-    // console.log(`Ничьи: ${draws}`);
-
 
 
