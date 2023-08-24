5d60073b76ef10dc2de5fdcd573723f0e90f933f
Dmytro Khodatskyi <khodatskyidmytro@gmail.com>
2023-08-24 15:23:26 +0300

    create  custom field

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -2,25 +2,33 @@
 
 // ждем пока загрузится страница
 document.addEventListener("DOMContentLoaded", function () {
-    grid()
+    grid(66)
 });
 
 
 const grid = (num = 16) => {
     const  divs = document.getElementsByClassName('divs')
+    const gridContainer = document.getElementById("grid-container");
+    
+    // получаем в аргументе выбор числа от пользователя и выставляем размеры поля по формуле 
+    const resolutionField = (num) => {
+        let widthField = Math.round(Math.sqrt(num))
+        widthField *= 50
+        gridContainer.style.width = widthField + 'px'
+    }
+    resolutionField(num)
 
-    // Создаем num кол-во дивов, num зависит от аргумента
+    // Создаем num кол-во дивов и добавляем их в нашу сетку
     for(let i = 0; i < num; i++) {
-        const gridContainer = document.getElementById("grid-container");
         const createNewDiv = document.createElement("div");
 
         createNewDiv.className = "divs"; // Устанавливаем class для нового div
         gridContainer.appendChild(createNewDiv);
     }
-    // к каждому элементу на который наводится мышка мы присваиваем новый класс
+    // к каждому элементу на который наводится мышка мы присваиваем новый класс в котором мы меняем цвет фона
     for (let i = 0; i < divs.length; i++) {
         divs[i].addEventListener('mouseenter', (e) => {
-            let enteredElement = e.target // e.target содержит элемент, на котором произошло событие
+            let enteredElement = e.target 
             enteredElement.classList.add('black')
         });
     }
