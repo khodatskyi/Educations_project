f75007c5e52ca0cdc5d6763e6fca256fe7dc81dd
Dmytro Khodatskyi <khodatskyidmytro@gmail.com>
2023-09-02 11:35:02 +0300

    add rules from password

--- 4 Sign-up Form/script.js
+++ 4 Sign-up Form/script.js
@@ -1,9 +1,20 @@
 const password = document.getElementById("password")
 const passwordConfirm = document.getElementById("password-confirm")
 const PasswordMismatch = document.getElementById("Password-mismatch")
+const imgInfo = document.getElementById('img-info');
+const paragraphRules = document.getElementById('password-rules')
+const upperLetter = document.getElementById('upper-letter')
+const number = document.getElementById('number')
+
+function hasUppercase(password) {
+    return /[A-Z]/.test(password);
+  }
+function hasNumber(password) {
+    return /[0-9]/.test(password);
+  }
 
 passwordConfirm.addEventListener('input', (e) => {
-
+    
     if(password.value == passwordConfirm.value) {
         PasswordMismatch.classList.remove('incorrect')
         PasswordMismatch.classList.add('correct')
@@ -15,3 +26,19 @@ passwordConfirm.addEventListener('input', (e) => {
         PasswordMismatch.textContent = "Incorrect passwords!"
     }
 })
+
+imgInfo.addEventListener('mouseenter', (e) => {
+    paragraphRules.classList.remove('hidden')
+    paragraphRules.classList.add('password-rules')
+    if (hasUppercase(password.value)) {
+        upperLetter.classList.add('correct')
+    } else upperLetter.classList.remove('correct')
+    if (hasNumber(password.value)) {
+        number.classList.add('correct')
+    } else number.classList.remove('correct')
+});
+
+imgInfo.addEventListener('mouseleave', () => {
+    paragraphRules.classList.add('hidden')
+    paragraphRules.classList.remove('password-rules')
+  });
